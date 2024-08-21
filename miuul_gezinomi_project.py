646b5528a46876db4a3fbf268e135988b33c83e1
Gamze Gezgin <gamzegezgin@yahoo.com>
2024-08-21 21:55:54 +0300

    deleted useless library

--- miuul_gezinomi_project.py
+++ miuul_gezinomi_project.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
 
 ########################################################################
 # GOREV-1
